16c04e94e8258ccd210d7a65693ee0bbc552d8f0
Aleksei Slesarev <lexsl84@gmail.com>
2018-08-14 12:53:23 +0700

    add function to calculate winner, check befor every move winner and available moves

--- src/index.js
+++ src/index.js
@@ -4,14 +4,33 @@ import './index.css';
 
 
 function Square(props) {
-    render() {
-        return (
-            <button className="square" onClick={props.onClick}>
-                {props.value}
-            </button>
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
         );
-    }
 }
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 
 class Board extends React.Component {
     constructor(props) {
@@ -24,6 +43,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O' ;
         this.setState({squares: squares, xIsNext: !this.state.xIsNext,});
     }
@@ -35,7 +57,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player:  ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player:  ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
 
         return (
             <div>

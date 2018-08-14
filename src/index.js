98af104e3c05341cc1578814cebdd7ea26237ae7
Aleksei Slesarev <lexsl84@gmail.com>
2018-08-14 12:29:28 +0700

    change Square from class to function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
+function Square(props) {
     render() {
         return (
-            <button className="square" onClick={() => this.props.onClick()}>
-                {this.props.value}
+            <button className="square" onClick={props.onClick}>
+                {props.value}
             </button>
         );
     }
